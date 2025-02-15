442a28a2ba100ce02bf95a6594dd9fa859f3489b
Nipitare <nipitareyt@gmail.com>
2025-02-15 13:44:06 +0200

    Update for canopy 1.3.8 and minecraft 1.21.60

--- Storage-Utilities (BP)/scripts/src/commands/SummonCommand.js
+++ Storage-Utilities (BP)/scripts/src/commands/SummonCommand.js
@@ -2,7 +2,7 @@ import {Command} from 'lib/canopy/CanopyExtension';
 import  extension from 'config'
 const BetterSummonCommand = new Command({
     name: 'summon',
-    description: 'Tells you the necessary bit grouping for any given number of bits 7-10.',
+    description: 'Summons any amount (up to 1,000) of a specified entity',
     usage: 'summon [entity] [amount]',
     callback: BetterSummonCommandCallback,
     args: [
