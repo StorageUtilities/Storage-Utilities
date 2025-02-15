9c6e8026c1c08ae1ad35dc2c70c36c98207aaa8a
Nipitare <nipitareyt@gmail.com>
2025-02-15 13:41:03 +0200

    Update for canopy 1.3.8 and minecraft 1.21.60

--- Storage-Utilities (BP)/scripts/config.js
+++ Storage-Utilities (BP)/scripts/config.js
@@ -2,7 +2,7 @@ import { CanopyExtension } from 'lib/canopy/CanopyExtension';
 const extension = new CanopyExtension({
     name: 'StorageUtilities',
     description: 'Storage Utilities is an addon aimed to improve the process of making a system related to storage tech.',
-    version: '0.0.1',
+    version: '1.1.1',
 });
 
 export default extension;
