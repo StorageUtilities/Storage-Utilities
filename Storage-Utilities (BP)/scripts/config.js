ada94c0cc7c5eef387ab26b2c36779a9ba5fe32f
Nipitare <nipitareyt@gmail.com>
2025-03-28 15:56:10 +0200

    Bump version

--- Storage-Utilities (BP)/scripts/config.js
+++ Storage-Utilities (BP)/scripts/config.js
@@ -2,7 +2,7 @@ import { CanopyExtension } from 'lib/canopy/CanopyExtension';
 const extension = new CanopyExtension({
     name: 'StorageUtilities',
     description: 'Storage Utilities is an addon aimed to improve the process of making a system related to storage tech.',
-    version: '1.1.1',
+    version: '1.1.2',
 });
 
 export default extension;
